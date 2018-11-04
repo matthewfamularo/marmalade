1b9e9b85e31f088bc3a5d15b2a8c1e969065ab0f
matthewfamularo <matthewfamularo@icloud.com>
2018-11-04 12:09:15 -0500

    fix padding bottom on Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Mix from "./Mix.js";
 const Home = ({ mixes, ...props }) => (
-  <div className="flex flex-wrap justify-between mixes ph5 ph4-l">
+  <div className="flex flex-wrap justify-between mixes ph5 ph4-l pb6 pad-bottom">
     {mixes.slice(0, 6).map(mix => (
       <div className="mix mb4">
         <Mix {...props} {...mix} id={mix.key} />
